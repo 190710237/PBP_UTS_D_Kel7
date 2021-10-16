f8538e798160f1d67a0838c9b1ab76f5daf0f0e3
ASUS <endertroller288@gmail.com>
2021-10-16 15:46:59 +0700

    ini untuk penambahan fragment

--- app/src/main/java/com/example/uts_pbp_d_kel7/MainActivity.java
+++ app/src/main/java/com/example/uts_pbp_d_kel7/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.uts_pbp_d_kel7;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.uts_pbp_d_kel7.Model.User;
+import com.example.uts_pbp_d_kel7.Preferences.UserPreferences;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private UserPreferences userPreferences;
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userPreferences = new UserPreferences(MainActivity.this);
+        user = userPreferences.getUserLogin();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.menu_list,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.about_us){
+
+        }else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure want to exit?")
+                    .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            userPreferences.logout();
+
+                            finishAndRemoveTask();
+                        }
+                    })
+                    .show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
